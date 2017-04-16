57452331af6c86941a9bd05acef3de59f3144c46
JakeGorilla <j255121@gmail.com>
2017-04-17 05:19:27 +0800

    20170417

--- public/home/project.js
+++ public/home/project.js
@@ -5,21 +5,36 @@ window.onhashchange = function (e) {
   console.log(e);
 };
 
-Vue.component('tab-entry',{
-  template: '<a v-for="tab in tablist" v-bind:id="tab.id" :href="#" + "tab.id">{{ tab.name }}</a>'
-});
+// Vue.component('tab-entry',{
+//   template: '<a v-for="tab in tablist" v-bind:id="tab.id" :href="#" + "tab.id">{{ tab.name }}</a>'
+// });
 
-var tabs = new Vue({
-  el: '#tabs',
-  data: {
-    tablist: [
-      { id: 'news', name: 'NEWS' },
-      { id: 'reports', name: 'Reports' },
-      { id: 'operational', name: 'Operational<br>Products' },
-      { id: 'modelfcst', name: 'Model/Forecast<br>Products' },
-      { id: 'research', name: 'Research<br>Products' },
-      { id: 'missions', name: 'Missions' },
-      { id: 'links', name: 'Links&amp;Tools' }
-    ]
-  }
-});
+// var tabs = new Vue({
+//   el: '#tabs',
+//   data: {
+//     tablist: [
+//       { id: 'news', name: 'NEWS' },
+//       { id: 'reports', name: 'Reports' },
+//       { id: 'operational', name: 'Operational<br>Products' },
+//       { id: 'modelfcst', name: 'Model/Forecast<br>Products' },
+//       { id: 'research', name: 'Research<br>Products' },
+//       { id: 'missions', name: 'Missions' },
+//       { id: 'links', name: 'Links&amp;Tools' }
+//     ]
+//   }
+// });
+
+var tabs = [];
+// console.log(tabs);
+document.querySelectorAll('.mdl-layout__tab').forEach(function(element,index) {
+  tabs.push(new Vue({
+    el: element,
+    data: {
+      activeFlag: (index == 1) ? true : false
+    }
+  }));
+}, this);
+
+tabs[0].$data['activeFlag'] = true;
+
+// MaterialLayoutTab(document.getElementById('reports'),document.querySelectorAll('.mdl-layout__tab'),document.querySelectorAll('.mdl-layout__tab-panel'),document.getElementsByClassName('mdl-layout'));
