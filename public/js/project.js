0c92c0c3ce49b68cfe2f2ac28895ce7d4a04c59a
JakeGorilla <j255121@gmail.com>
2017-04-24 19:59:24 +0800

    add firebaseui for auth

--- public/js/project.js
+++ public/js/project.js
@@ -2,6 +2,17 @@
 var panels = {};  // contains names and Vue instances for tab contents
 var tabNames = [];  // names available tabs
 var keyFunctions = {};  // drawer functions name(hashtag) with Vue inst to sync state
+var userName, userEmail, userEmailVerified, userPhotoURL, userId, userProviderData; // firebase
+var singleFunctions = {
+  singinSuccess: function () {
+    // console.log('in success');
+    singleFunctions['dimOff']();
+    keyFunctions['login'].loginState = true;
+  },
+  dimOff: function () {
+    document.getElementById('dim').style.display = 'none';
+  }
+};
 
 // Initialize Firebase
 var config = {
@@ -12,6 +23,24 @@ var config = {
   storageBucket: "scstimx-b5bb8.appspot.com"
 };
 firebase.initializeApp(config);
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    var userName = user.displayName;
+    var userEmail = user.email;
+    var userEmailVerified = user.emailVerified;
+    var userPhotoURL = user.photoURL;
+    var userId = user.uid;
+    var userProviderData = user.providerData;
+  } else {
+    userName = undefined;
+    userEmail = undefined;
+    userEmailVerified = undefined;
+    userPhotoURL = undefined;
+    userId = undefined;
+    userProviderData = undefined;
+  }
+});
 
 // for querySelectorAll
 // forEach method, could be shipped as part of an Object Literal/Module
@@ -89,18 +118,69 @@ forEach(document.querySelectorAll('.mdl-layout__tab'), function (element, index)
 //   // loadingPanel.style.display = 'none';
 // }
 
-// init drawer functions
+// init login functions
+// FirebaseUI config.
+var uiConfig = {
+  signInSuccessUrl: '#singinSuccess',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: '#accountTOS',
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  signInFlow: 'popup',
+};
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+
+// login button
 keyFunctions['login'] = new Vue({
   el: document.querySelector('.func[href="#login"]'),
   data: {
-    state: 'Not login',
-    availableStates: ['Not login'],
-    message: 'Login'
+    loginState: false, // true for logged in, false for not
+    message: '',
+    s2m: {
+      true: 'Logout',
+      false: 'Login',
+    },
   },
   methods: {
     trigger: function () {
       // @TODO: Login system
-      console.log('a');
+      if (this.loginState) {
+        // logout sequence
+        // console.log("trigger out");
+        firebase.auth().signOut().then(function () {
+          // Sign-out successful.
+          keyFunctions['login'].loginState = false;
+        }).catch(function (error) {
+          // An error happened.
+        });
+      } else {
+        //  login sequence
+        // console.log("trigger in");
+        ui.start('#firebaseui-auth-container', uiConfig);
+        document.getElementById('dim').style.display = 'block';
+      }
+    }
+  },
+  beforeMount: function () {
+    if (userName) {
+      console.log("beforeMount");
+      this.loginState = true;
+    }
+    this.message = this.s2m[this.loginState];
+  },
+  watch: {
+    loginState: function () {
+      console.log('dec');
+      this.message = this.s2m[this.loginState]
     }
   }
 });
@@ -114,6 +194,7 @@ if (!window.HashChangeEvent) (function () {
     lastURL = document.URL;
   });
 }());
+
 // handle hashtag change for 4 case
 // 1. user change url -> change activeFlag -> jump to new tab
 // 2. user clicked tab -> tab had chaned -> update activeFlag
@@ -133,6 +214,8 @@ window.onhashchange = function (e) {
     if (keyFunctions[after] && keyFunctions[after].hasOwnProperty('trigger')) {
       // case 3
       keyFunctions[after].trigger();
+    } else if (singleFunctions[after]) {
+      singleFunctions[after]();
     }
     return (0);
   }
@@ -150,3 +233,4 @@ window.onhashchange = function (e) {
   tabs[after].activeFlag = true;
   // loadingPanel.style.display = 'none';
 };
+
